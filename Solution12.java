4ce4f1b5d2218eb63a7d06546649a7c7a6429c02
rupsikha <rupsikha007@gmail.com>
2016-09-30 16:14:43 +0530

    Add files via upload

---
+++ Solution12.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+public class Solution12
+{
+	public static void main(String args[])
+	{
+Scanner in=new Scanner(System.in);
+String s=in.next();
+String st=in.next();
+Without m=new Without();
+String str=m.withoutString(s,st);
+System.out.println(str);
+}
+}
---
+++ Without.java
@@ -0,0 +1,6 @@
+public class Without {
+	public String withoutString(String a,String b) {
+	return a.replace(b,"");
+}
+}	
+		
